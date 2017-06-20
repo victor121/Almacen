1be9baa890fc8434b6a9d2538672851c93098b53
victor121 <victor.gomez28@inacapmail.cl>
2017-06-20 17:27:27 -0400

    Trabajo

---
+++ Almacen/IngresarDineroForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Almacen
+{
+    public partial class IngresarDineroForm : Form
+    {
+        public IngresarDineroForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresarDinero_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int plata = Convert.ToInt32(txtIngresarDinero.Text);
+                var caja = new caja();
+                caja.ingresarDinero(plata);
+                MessageBox.Show("Dinero agregado exitosamente");
+                txtIngresarDinero.Text = "";
+            }
+            catch (Exception ex)
+            {
+            }
+            
+        }
+
+        private void IngresarDineroForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Almacen/_caja.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen
+{
+    public partial class caja
+    {
+        public void ingresarDinero(int plata)
+        {
+            var bd = new BD();
+            var tablaCaja = bd.caja;
+            foreach (var caja in tablaCaja)
+            {           
+                caja.dinero = caja.dinero + plata;
+            }
+            bd.SaveChanges();
+        }
+        public bool sacarDinero(int plata)
+        {
+            var bd = new BD();
+            var tablaCaja = bd.caja;
+            foreach(var caja in tablaCaja)
+            {
+                if(plata<=caja.dinero)
+                {
+                    caja.dinero = caja.dinero - plata;
+                    bd.SaveChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+            bd.SaveChanges();
+        }
+    }
+}
